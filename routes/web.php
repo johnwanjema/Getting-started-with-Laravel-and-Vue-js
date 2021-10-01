2b3f484a8a40bc3c23d2d143464f303d3c27787d
wanjema <jonwanjema@gmail.com>
2021-10-01 11:03:47 +0300

    update laravel route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any?}', [
+    function () {
+        return view('welcome');
+    }
+])->where('any', '.*')  ;
